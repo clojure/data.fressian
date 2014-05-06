5f90a9df1ad9eed3740f9179e0c171ae6f860dfc
Alex Miller <alex@puredanger.com>
2014-05-06 15:17:47 -0500

    add project.clj for dev

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.clojure/data.fressian "0.2.1-SNAPSHOT"
+  :description "Read/write Fressian from Clojure."
+  :url "https://github.com/clojure/data.fressian"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.fressian/fressian "0.6.5"]
+                 [org.clojure/test.generative "0.5.1" :scope "test"]]
+  :jvm-opts ["-Xmx2g" "-server"])
