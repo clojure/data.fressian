401ff5bb450f6dffe8c43c45b52ae64553b382ef
puredanger <alex.miller@cognitect.com>
2015-08-03 09:52:51 -0500

    update to latest fressian version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.fressian/fressian "0.6.5"]
+                 [org.fressian/fressian "0.6.6"]
                  [org.clojure/test.generative "0.5.1" :scope "test"]]
   :jvm-opts ["-Xmx2g" "-server"])
