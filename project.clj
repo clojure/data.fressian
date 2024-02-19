fac56aa73a31ed64a44498971844f7eea420fa1f
Alex Miller <alex.miller@cognitect.com>
2024-02-19 13:26:30 -0600

    update parent pom and clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clojure/data.fressian"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.fressian/fressian "0.6.6"]
-                 [org.clojure/test.generative "0.5.1" :scope "test"]]
+                 [org.clojure/test.generative "1.1.1" :scope "test"]]
   :jvm-opts ["-Xmx2g" "-server"])
