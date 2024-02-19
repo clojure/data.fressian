220f45a3017001db0be2c65c27f553cf0a1b397c
Alex Miller <alex.miller@cognitect.com>
2024-02-19 15:35:09 -0600

    fix test.generative version

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.fressian/fressian "0.6.6"]
-                 [org.clojure/test.generative "1.1.1" :scope "test"]]
+                 [org.fressian/fressian "0.6.8"]
+                 [org.clojure/test.generative "1.1.0" :scope "test"]]
   :jvm-opts ["-Xmx2g" "-server"])
